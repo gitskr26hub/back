e0fe32d186225a833913f9fa3991e6c8656ab5c4
unknown <kumarsugam2609@gmail.com>
2022-12-12 06:27:03 +0530

    .

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 require('dotenv').config()
 
-const connection = mongoose.connect(mongoDB_url)
+const connection = mongoose.connect(process.env.mongoDB_url)
 
 
 module.exports = {
